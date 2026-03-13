733230d3bb6446c077cbf7bdf4c0978cb5562b1c
dimovdmitrii <dimovdima1509@gmail.com>
2026-03-13 22:48:51 +0100

    words.json controle

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.77abqnenhhg"
+    "revision": "0.0oic9fo9kp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
